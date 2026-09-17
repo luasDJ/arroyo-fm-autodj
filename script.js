0c5005aaf2c143acffa03fc136ec1d4ea8839f35
luasDJ <lucassanchezesteban@gmail.com>
2026-09-17 12:51:02 +0000

    feat: restaurar playlist y programacion con canciones de la coleccion

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const adminCode = Array.from({ length: 7 }, (_, index) => String.fromCharCode(49 + index)).join("");
 const playlistSource = [
-  { title: "Sunset Drive", artist: "Nova Echo", duration: "3:42", mood: "Synthwave", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" },
-  { title: "Blue Horizon", artist: "North & Sea", duration: "4:05", mood: "Indie pop", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" },
-  { title: "Neon Lights", artist: "Mira Vale", duration: "3:28", mood: "Dance", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3" },
-  { title: "Midnight Route", artist: "Silver Coast", duration: "4:19", mood: "Electronic", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3" },
-  { title: "Open Sky", artist: "Luma Tide", duration: "3:53", mood: "Acoustic", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3" },
-  { title: "Night Signal", artist: "Blue Harbor", duration: "3:40", mood: "Dream pop", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3" },
-  { title: "Velvet Echo", artist: "Glass Harbor", duration: "4:00", mood: "Alternative", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3" },
-  { title: "City Pulse", artist: "Electric Avenue", duration: "3:51", mood: "House", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3" }
+  { title: "NUEVAYoL (Clean Version)", artist: "Bad Bunny", duration: "3:00", mood: "Urban", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" },
+  { title: "Shape of You", artist: "Ed Sheeran", duration: "3:00", mood: "Pop", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" },
+  { title: "Despacito (Remix)", artist: "Luis Fonsi & Daddy Yankee & Justin Bieber", duration: "3:00", mood: "Latino", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3" },
+  { title: "Don't Start Now (Lyrics)", artist: "Dua Lipa", duration: "3:00", mood: "Dance", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3" },
+  { title: "Pepas (LetraLyrics)", artist: "Farruko", duration: "3:00", mood: "Reggaeton", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3" },
+  { title: "Rude (Lyrics)", artist: "MAGIC!", duration: "3:00", mood: "Reggae", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3" },
+  { title: "Potra Salvaje (Hard Remix)", artist: "Isabel Aaiún", duration: "3:00", mood: "Remix", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3" },
+  { title: "HIT THE JACKPOT! [Official Music Video]", artist: "GameboyJones", duration: "3:00", mood: "Electro", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3" },
+  { title: "La Graciosa", artist: "Arroyo FM", duration: "3:00", mood: "AutoDJ", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3" },
+  { title: "Baile inolvidable", artist: "Arroyo FM", duration: "3:00", mood: "AutoDJ", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3" }
 ];
 
 const programmingSource = [
